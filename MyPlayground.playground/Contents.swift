fe13d60fce6f1810ad90f2d7a5bea4a7deff31fd
Mo74mm3d <mohammedjalqattan@icloud.com>
2022-03-19 10:50:31 +0300

    Done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,7 +10,12 @@ struct Student{
 var mohammed = Student(name: "Mohammed", age: 16, grade: 100, attendance: true)
 var ahmad = Student(name: "Ahmad", age: 15, grade: 90, attendance: false)
 var hamad = Student(name: "hamad", age: 17, grade: 85, attendance: true)
-print(mohammed)
-print(ahmad)
-print(hamad)
 
+let student = [
+    Student(name: "Mohammed", age: 16, grade: 100, attendance: true),
+    Student(name: "Ahmad", age: 15, grade: 90, attendance: false),
+    Student(name: "hamad", age: 17, grade: 85, attendance: true)
+]
+
+print(student[0].age, student[1].attendance)
+print(student[2].name)
