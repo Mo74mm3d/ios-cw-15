2ca232a0e29d58edffa421394c5c3201b0b054d4
Mo74mm3d <mohammedjalqattan@icloud.com>
2022-03-19 10:32:22 +0300

    Done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Student{
+    var name: String
+    var age: Int
+    var grade: Double
+    var attendance: Bool
+}
+
+var Mohammed = Student(name: "Mohammed", age: 16, grade: 100, attendance: true)
+var Ahmad = Student(name: "Ahmad", age: 15, grade: 90, attendance: false)
+
+
